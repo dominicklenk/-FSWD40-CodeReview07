bf50810307b19d3e114e4e78a56335d8ad2f69ae
dominicklenk <dominic_klenk@yahoo.at>
2018-05-26 16:55:03 +0200

    add files

--- script.js
+++ script.js
@@ -1,6 +1,80 @@
-var Location = /** @class */ (function () {
-    function Location(argument) {
-        // code...
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Mylocation = /** @class */ (function () {
+    function Mylocation(title, city, zip, address, teaserimg, creationDate) {
+        this.title = title;
+        this.city = city;
+        this.zip = zip;
+        this.address = address;
+        this.teaserimg = teaserimg;
+        this.creationDate = creationDate;
     }
-    return Location;
+    Mylocation.prototype.render = function () {
+        document.querySelector('.hugo').innerHTML += "\n\t<section id=\"bloglocation\">\n\t\t\t<div class=\"container\">\n\t            <div class=\"row\">\n\t                <div class=\"col-md-8 col-md-offset-2 text-center\">\n\t                    <h2 class=\"sec-title\">Places</h2>\n\t                </div>\n\t            </div>\n\t            <div class=\"row\">\n\t\t<div class=\"col-lg-3 col-md-6 col-sm-12\">\n            <div class=\"card\">\n                <div class=\"card-header text-center\">\n                    " + this.title + "\n                </div>\n                <img class=\"card-img-top\" style=\"max-width: 100%\" src=\"img/" + this.teaserimg + "\" alt=\"" + this.title + "\">\n                <div class=\"card-body\">\n                    <p>" + this.address + ", " + this.zip + " " + this.city + "</p>\n                    <hr><p>Created: " + this.creationDate + "</p>\n                </div>\n            </div>\n        </div>\n      </div>\n\t</div>\n</section>";
+    };
+    ;
+    return Mylocation;
 }());
+;
+var church = new Mylocation('St. Charles Church', 'Vienna', 1010, 'Karlsplatz 1', "charles-church.jpg", new Date('2017-02-08 08:30'));
+var zoo = new Mylocation('Zoo Schönbrunn', 'Vienna', 1130, 'Maxingstraße 13b', 'zoo-vienna.jpg', new Date('2018-04-08 10:30'));
+var Restaurants = /** @class */ (function (_super) {
+    __extends(Restaurants, _super);
+    function Restaurants(title, city, zip, address, teaserimg, creationDate, tel, kindof, site) {
+        var _this = _super.call(this, title, city, zip, address, teaserimg, creationDate) || this;
+        _this.tel = tel;
+        _this.kindof = kindof;
+        _this.site = site;
+        _this.tel = tel,
+            _this.kindof = kindof,
+            _this.site = site;
+        return _this;
+    }
+    ;
+    Restaurants.prototype.render = function () {
+        document.querySelector('.hugo').innerHTML += "\n\t\t<section id=\"bloglocation\">\n\t\t\t<div class=\"container\">\n\t            <div class=\"row\">\n\t                <div class=\"col-md-8 col-md-offset-2 text-center\">\n\t                    <h2 class=\"sec-title\">Restanrants</h2>\n\t                </div>\n\t            </div>\n\t            <div class=\"row\">\n\t\t\t\t<div class=\"col-lg-3 col-md-6 col-sm-12\">\n\t\t            <div class=\"card\">\n\t\t                <div class=\"card-header text-center\">\n\t\t                    " + this.title + "\n\t\t                </div>\n\t\t                <img class=\"card-img-top\" style=\"max-width: 100%\" src=\"img/" + this.teaserimg + "\" alt=\"" + this.title + "\">\n\t\t                <div class=\"card-body\">\n\t\t                    <p>" + this.address + ", " + this.zip + " " + this.city + "</p>\n\t\t                    <p>" + this.tel + "</p>\n\t\t                    <p>" + this.site + "</p>\n\t\t                    <p>Type: " + this.kindof + "</p>\n\t\t                    <hr><p>Created: " + this.creationDate + "</p>\n\t\t                </div>\n\t\t            </div>\n\t\t        </div>\n\t\t      </div>\n\t\t    </div>\n\t\t </section>";
+    };
+    ;
+    return Restaurants;
+}(Mylocation));
+;
+var thai = new Restaurants('Lemon Leaf Thai Restaurant', 'Vienna', 1050, 'Kettenbrückengasse 19', 'lemon-leaf.png', new Date('2018-05-04 18:00'), '+43(1)5812308', 'thai', 'http://www.lemonleaf.at');
+var sixta = new Restaurants('SIXTA', 'Vienna', 1050, 'Schönbrunner Straße 21', 'sixta.png', new Date('2018-01-04 17:00'), '+43 1 58 528 56 / +43 1 58 528 56', 'wienerisch', 'http://www.sixta-restaurant.at/');
+var Events = /** @class */ (function (_super) {
+    __extends(Events, _super);
+    function Events(title, city, zip, address, teaserimg, creationDate, eventDate, eventTime, price) {
+        var _this = _super.call(this, title, city, zip, address, teaserimg, creationDate) || this;
+        _this.eventDate = eventDate;
+        _this.eventTime = eventTime;
+        _this.price = price;
+        _this.eventDate = eventDate,
+            _this.eventTime = eventTime,
+            _this.price = price;
+        return _this;
+    }
+    ;
+    Events.prototype.render = function () {
+        document.querySelector('.hugo').innerHTML += "\n\t\t<section id=\"bloglocation\">\n\t\t\t<div class=\"container\">\n\t            <div class=\"row\">\n\t                <div class=\"col-md-8 col-md-offset-2 text-center\">\n\t                    <h2 class=\"sec-title\">Events</h2>\n\t                </div>\n\t            </div>\n\t            <div class=\"row\">\n\t\t\t\t\t<div class=\"col-lg-3 col-md-6 col-sm-12\">\n\t\t\t            <div class=\"card\">\n\t\t\t                <div class=\"card-header text-center\">\n\t\t\t                    " + this.title + "\n\t\t\t                </div>\n\t\t\t                <img class=\"card-img-top\" style=\"max-width: 100%\" src=\"img/" + this.teaserimg + "\" alt=\"" + this.title + "\">\n\t\t\t                <div class=\"card-body\">\n\t\t\t                    <p>" + this.address + ", " + this.zip + " " + this.city + "</p>\n\t\t\t                    <p>" + this.eventDate + " " + this.eventTime + "</p>\n\t\t\t                    <p>" + this.price + "</p>\n\t\t\t                    <hr><p>Created: " + this.creationDate + "</p>\n\t\t\t                </div>\n\t\t\t            </div>\n\t\t\t        </div>\n\t\t      \t</div>\n\t\t    </div>\n\t\t </section>";
+    };
+    ;
+    return Events;
+}(Mylocation));
+;
+var kris = new Events('Kris Kristofferson', 'Vienna', 1150, 'Wiener Stadthalle, Halle F, Roland Rainer Platz 1', 'kris-kristofferson.jpg', new Date('2017-12-18 19:00'), 'Fr., 15.06.2018', '20:00', '58,50 EUR');
+var lenny = new Events('Lenny Kravitz', 'Vienna', 1150, 'Wiener Stadthalle, Halle D, Roland Rainer Platz 1', 'lenny-kravitz.jpg', new Date('2017-12-14 13:00'), 'Sat, 09.06.2018', '19:30', '€ 47,80');
+var blogs = [church, zoo, thai, sixta, kris, lenny];
+function displayBlog(arr) {
+    arr.forEach(function (blog) {
+        blog.render();
+    });
+}
+;
+displayBlog(blogs);
--- script.ts
+++ script.ts
@@ -1,6 +1,134 @@
-class Location {
+class Mylocation {
 	
-	constructor(argument) {
-		// code...
-	}
-}
+	constructor(public title: string, public city: string, public zip: number, public address: string, public teaserimg: string, public creationDate: Date) {}
+	render(){
+	document.querySelector('.hugo').innerHTML += `
+	<section id="bloglocation">
+			<div class="container">
+	            <div class="row">
+	                <div class="col-md-8 col-md-offset-2 text-center">
+	                    <h2 class="sec-title">Places</h2>
+	                </div>
+	            </div>
+	            <div class="row">
+		<div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card">
+                <div class="card-header text-center">
+                    ${this.title}
+                </div>
+                <img class="card-img-top" style="max-width: 100%" src="img/${this.teaserimg}" alt="${this.title}">
+                <div class="card-body">
+                    <p>${this.address}, ${this.zip} ${this.city}</p>
+                    <hr><p>Created: ${this.creationDate}</p>
+                </div>
+            </div>
+        </div>
+      </div>
+	</div>
+</section>`
+};
+};
+
+let church = new Mylocation('St. Charles Church', 'Vienna', 1010, 'Karlsplatz 1', "charles-church.jpg", new Date('2017-02-08 08:30'));
+let zoo = new Mylocation('Zoo Schönbrunn', 'Vienna', 1130, 'Maxingstraße 13b', 'zoo-vienna.jpg', new Date('2018-04-08 10:30'));
+
+
+class Restaurants extends Mylocation {
+	constructor(title: string, city: string, zip: number, address: string, teaserimg: string, creationDate: Date, public tel: string, public kindof: string, public site: string){
+	super(title, city, zip, address, teaserimg, creationDate);
+	this.tel = tel,
+	this.kindof = kindof,
+	this.site = site
+	};
+	render(){
+		document.querySelector('.hugo').innerHTML += `
+		<section id="bloglocation">
+			<div class="container">
+	            <div class="row">
+	                <div class="col-md-8 col-md-offset-2 text-center">
+	                    <h2 class="sec-title">Restanrants</h2>
+	                </div>
+	            </div>
+	            <div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-12">
+		            <div class="card">
+		                <div class="card-header text-center">
+		                    ${this.title}
+		                </div>
+		                <img class="card-img-top" style="max-width: 100%" src="img/${this.teaserimg}" alt="${this.title}">
+		                <div class="card-body">
+		                    <p>${this.address}, ${this.zip} ${this.city}</p>
+		                    <p>${this.tel}</p>
+		                    <p>${this.site}</p>
+		                    <p>Type: ${this.kindof}</p>
+		                    <hr><p>Created: ${this.creationDate}</p>
+		                </div>
+		            </div>
+		        </div>
+		      </div>
+		    </div>
+		 </section>`
+
+	};
+};
+
+
+let thai = new Restaurants('Lemon Leaf Thai Restaurant', 'Vienna', 1050, 'Kettenbrückengasse 19', 'lemon-leaf.png', new Date('2018-05-04 18:00'), '+43(1)5812308', 'thai', 'http://www.lemonleaf.at');
+let sixta = new Restaurants('SIXTA', 'Vienna', 1050, 'Schönbrunner Straße 21', 'sixta.png', new Date('2018-01-04 17:00'), '+43 1 58 528 56 / +43 1 58 528 56', 'wienerisch', 'http://www.sixta-restaurant.at/');
+
+
+
+class Events extends Mylocation {
+	constructor(title, city, zip, address, teaserimg, creationDate, public eventDate: string, public eventTime: string, public price: string){
+		super(title, city, zip, address, teaserimg, creationDate);
+		this.eventDate = eventDate,
+		this.eventTime = eventTime,
+		this.price = price
+	};
+	render(){
+		document.querySelector('.hugo').innerHTML += `
+		<section id="bloglocation">
+			<div class="container">
+	            <div class="row">
+	                <div class="col-md-8 col-md-offset-2 text-center">
+	                    <h2 class="sec-title">Events</h2>
+	                </div>
+	            </div>
+	            <div class="row">
+					<div class="col-lg-3 col-md-6 col-sm-12">
+			            <div class="card">
+			                <div class="card-header text-center">
+			                    ${this.title}
+			                </div>
+			                <img class="card-img-top" style="max-width: 100%" src="img/${this.teaserimg}" alt="${this.title}">
+			                <div class="card-body">
+			                    <p>${this.address}, ${this.zip} ${this.city}</p>
+			                    <p>${this.eventDate} ${this.eventTime}</p>
+			                    <p>${this.price}</p>
+			                    <hr><p>Created: ${this.creationDate}</p>
+			                </div>
+			            </div>
+			        </div>
+		      	</div>
+		    </div>
+		 </section>`
+	};
+};
+
+
+let kris = new Events('Kris Kristofferson', 'Vienna', 1150, 'Wiener Stadthalle, Halle F, Roland Rainer Platz 1', 'kris-kristofferson.jpg', new Date('2017-12-18 19:00'), 'Fr., 15.06.2018', '20:00', '58,50 EUR');
+let lenny = new Events('Lenny Kravitz', 'Vienna', 1150, 'Wiener Stadthalle, Halle D, Roland Rainer Platz 1', 'lenny-kravitz.jpg', new Date('2017-12-14 13:00'), 'Sat, 09.06.2018', '19:30', '€ 47,80');
+let blogs: any[] = [church, zoo, thai, sixta, kris, lenny];
+
+
+
+
+
+
+function displayBlog(arr: any[]){
+    arr.forEach((blog:any)=>{
+        blog.render();
+    });
+};
+displayBlog(blogs);
+
